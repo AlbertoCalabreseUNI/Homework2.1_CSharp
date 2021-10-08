cee343f2134da2bb03de6a5a0eb60af5b59114c9
Alberto Calabrese <calabrese.1709532@studenti.uniroma1.it>
2021-10-08 09:57:50 +0200

    Added Online Mean Algorithm.

--- Homework2_CSharp/Form1.cs
+++ Homework2_CSharp/Form1.cs
@@ -12,9 +12,38 @@ namespace Homework2_CSharp
 {
     public partial class Form1 : Form
     {
+        private Random random;
+        private Timer timer;
+        public double mean { get; set; }
+        public int n { get; set; }
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+            timer = new Timer();
+
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 400;
+        }
+
+        private void button1_Click(object sender, EventArgs e) { timer.Start(); }
+
+        private void button2_Click(object sender, EventArgs e) { timer.Stop(); }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int generatedValue = random.Next(18, 31);
+            updateMean(generatedValue);
+            this.textBox1.AppendText("Generated Number: " + generatedValue);
+            this.textBox1.AppendText(Environment.NewLine);
+            this.textBox1.AppendText("New mean = " + mean.ToString());
+            this.textBox1.AppendText(Environment.NewLine);
+        }
+
+        private void updateMean(int nextNumber)
+        {
+            n += 1;
+            mean = mean + ((1.0d/n) * (nextNumber - mean));
         }
     }
 }
